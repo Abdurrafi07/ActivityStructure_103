2a0035e981dd4de1a13e54b2c57aebe39d808508
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-04-03 10:34:58 +0700

    Menampilkan mhs2

--- ActivityStructure_103/ActivityStructure_103.cpp
+++ ActivityStructure_103/ActivityStructure_103.cpp
@@ -10,10 +10,20 @@ struct mahasiswa
 };
 
 int main() {
-	mahasiswa mhs1;
+	mahasiswa mhs1, mhs2;
 	mhs1.NIM = "20210140019";
 	mhs1.nama = "Ardian Mulya";
 	mhs1.alamat = "Wonosobo";
 	mhs1.umur = 20;
+
+	cout << "Masukan NIM : ";
+	cin >> mhs2.NIM;
+	cout << "Masukan Nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukan Alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukan Umur : ";
+	cin >> mhs2.umur;
+
 
 }
